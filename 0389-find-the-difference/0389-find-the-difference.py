aba9da061f044e7bc879870d64b26b5c590d2f3b
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-10 12:48:41 +0530

    Time: 0 ms (100%), Space: 17.7 MB (92.73%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,6 +2,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
       dici1=Counter(s)
       dici2=Counter(t)
-      for ch in dici2:
-        if dici2[ch] !=dici1.get(ch,0):
-            return (ch)  
+      for char in dici2:
+        if dici2[char] !=dici1.get(char,0):
+            return (char)  
