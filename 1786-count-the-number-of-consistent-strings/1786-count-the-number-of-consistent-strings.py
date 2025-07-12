36ab67d3be78b586666b221613b44c3a75b1696f
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-12 08:46:19 +0530

    Time: 236 ms (33.38%), Space: 19.7 MB (44.71%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -1,10 +1,8 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        dici=Counter({ch:float("inf") for ch in allowed})
-        count=0      
-        for i in range(len(words)):
-            word=words[i]
-            dici2=Counter(word)
-            if dici>= dici2:
-                count=count+1
-        return (count)
+       string=set(allowed)
+       count=0
+       for word in words:
+            if all(ch in string for ch in word):
+                 count=count+1
+       return (count)
