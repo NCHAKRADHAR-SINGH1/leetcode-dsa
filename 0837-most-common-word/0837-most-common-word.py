45465a7e32f10b2fa02a2cedcd559509f6ca1a28
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-14 19:28:00 +0530

    Time: 4 ms (58.14%), Space: 12.4 MB (89.15%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.py
+++ 0837-most-common-word/0837-most-common-word.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
         for p in "!?',;.":
-           paragraph = paragraph.replace(p, " ")
-        para=paragraph.split()
-        para1=" ".join(i.lower().strip(" .,!") for i in para)
-        para2=para1.split()
+          paragraph= paragraph.replace(p," ")
+        para2=paragraph.lower().split()
         dici={}
         for r in range(len(para2)):
            dici[para2[r]]=1+dici.get(para2[r],0)
