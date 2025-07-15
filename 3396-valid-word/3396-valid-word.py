a491ce33697962ae6216504291110f98723ed09e
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-15 17:33:19 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.18%) - LeetHub

--- 3396-valid-word/3396-valid-word.py
+++ 3396-valid-word/3396-valid-word.py
@@ -3,16 +3,13 @@ class Solution:
         vowels={'a','e','i','o','u','A','E','I','O','U'}
         if len(word) < 3:
             return False
-        is_digit=True
         is_vowel=False
         is_consonant=False
         for char in word:
             if not (char.isdigit() or char.isalpha()):
                  return False
-            if char.isdigit():
-                is_digit=True
             elif char in vowels:
                 is_vowel=True
             elif char.isalpha():
                 is_consonant=True
-        return (is_digit and is_vowel and is_consonant)
+        return ( is_vowel and is_consonant)
