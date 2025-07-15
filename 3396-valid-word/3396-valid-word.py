fb4801e95a39f53e43071ff1721eb39ad1f95455
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-15 17:24:36 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.06%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        vowels={'a','e','i','o','u','A','E','I','O','U'}
+        if len(word) < 3:
+            return False
+        is_digit=True
+        is_vowel=False
+        is_consonant=False
+        for char in word:
+            if not (char.isdigit() or char.isalpha()):
+                 return False
+            if char.isdigit():
+                is_digit=True
+            elif char in vowels:
+                is_vowel=True
+            elif char.isalpha():
+                is_consonant=True
+        return (is_digit and is_vowel and is_consonant)
