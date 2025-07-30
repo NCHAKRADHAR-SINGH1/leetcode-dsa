11d9450a2bd03dc11c0944dcc96c86f4f3ac67a2
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-07-30 19:16:25 +0530

    Time: 11 ms (90.15%), Space: 17.9 MB (54.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dici1=Counter(ransomNote)
+        dici2=Counter(magazine)
+        return all(dici1[ch] <= dici2.get(ch,0) for ch in dici1 )
+            
