f786efe2e5629bbdd73d9d95b9fc25ffbb3655f1
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-08-07 18:38:02 +0530

    Time: 12 ms (31.68%), Space: 18 MB (43.29%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.py
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -4,8 +4,7 @@ class Solution:
         dici1=Counter(words[0])
         for i in range(1,len(words)):
             dici2=Counter(words[i])
-            for ch in dici1:
-                dici1[ch]=min(dici1.get(ch,0),dici2.get(ch,0))
+            dici1={ch:min(dici1.get(ch,0),dici2.get(ch,0)) for ch in dici1}
         li=[]
         for key,value in dici1.items():
             li.extend([key]*value)
