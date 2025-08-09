86c984102ce00c76efbfd76b5f658c1e5a05a800
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-08-09 10:21:10 +0530

    Time: 6 ms (26.81%), Space: 18 MB (13.4%) - LeetHub

---
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        seen=set([int(i) for i in s if i.isdigit()])
+        li=list((seen)) 
+        if len(li) < 2:
+            return -1
+        k=1
+        heap=[]
+        for i in li:
+            heapq.heappush(heap,-i)
+        if len(li) >= 2:
+            for i in range(k):
+                heapq.heappop(heap)
+        return (-heapq.heappop(heap))
+
+         
+      
