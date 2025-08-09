5ae54dac4b101b01f1e8f2cfafad8424f74f8bbf
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-08-09 10:31:09 +0530

    Time: 8 ms (7.41%), Space: 17.7 MB (53.71%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -7,11 +7,16 @@ class Solution:
         k=1
         heap=[]
         for i in li:
-            heapq.heappush(heap,-i)
+            heapq.heappush(heap,-i) #[-3,-2,-1],the heappush contains the first element with smallest ,remaining unorder
         if len(li) >= 2:
             for i in range(k):
-                heapq.heappop(heap)
-        return (-heap[0])
+                heapq.heappop(heap)#[-3],it removes the smallest ,After heapq.heappop(heap):
+                                   #Removes -3 (smallest value, i.e. largest positive number 3).
+
+                                   #Python now needs to reheapify to keep the smallest element at index 0.
+
+                                   #Between -1 and -2, the smallest is -2, so it becomes the new root.
+        return (-heap[0]) 
 
          
       
