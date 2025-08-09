a95042ca73938eeadf6759397591320ded248c19
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-08-09 20:49:33 +0530

    Time: 15 ms (20.91%), Space: 17.9 MB (42.63%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.py
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -1,13 +1,14 @@
-from collections import Counter
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
+        res=[]
         dici1=Counter(words[0])
         for i in range(1,len(words)):
             dici2=Counter(words[i])
-            dici1={ch:min(dici1.get(ch,0),dici2.get(ch,0)) for ch in dici1}
-        li=[]
+            for ch in dici1:
+                dici1[ch]=min(dici1[ch],dici2[ch])
         for key,value in dici1.items():
-            li.extend([key]*value)
-        return li
+            res.extend([key]*value)
+        return res
 
-      
+        
+        r
